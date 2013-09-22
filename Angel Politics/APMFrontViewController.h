424a233597043455f5f5ab6bef85eeec80c69202
panchito <francisco.ochoah@gmail.com>
2013-09-22 09:29:42 -0500

    Add FrontViewController

---
+++ Angel Politics/APMFrontViewController.h
@@ -0,0 +1,13 @@
+//
+//  APMFrontViewController.h
+//  Angel Politics
+//
+//  Created by Francisco on 22/09/13.
+//  Copyright (c) 2013 angelpolitics. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface APMFrontViewController : UIViewController
+
+@end
