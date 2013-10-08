c9ef94340641c9b992c221499167f309aa33d226
panchito <francisco.ochoah@gmail.com>
2013-10-07 21:27:38 -0500

    APMFrontCell

---
+++ Angel Politics/APMFrontCell.h
@@ -0,0 +1,13 @@
+//
+//  APMFrontCell.h
+//  Angel Politics
+//
+//  Created by Francisco on 7/10/13.
+//  Copyright (c) 2013 angelpolitics. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface APMFrontCell : UITableViewCell
+
+@end
---
+++ Angel Politics/APMFrontCell.m
@@ -0,0 +1,29 @@
+//
+//  APMFrontCell.m
+//  Angel Politics
+//
+//  Created by Francisco on 7/10/13.
+//  Copyright (c) 2013 angelpolitics. All rights reserved.
+//
+
+#import "APMFrontCell.h"
+
+@implementation APMFrontCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
